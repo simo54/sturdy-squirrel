65ddad44aec0786940e6fa39339de7053991a15b
kalyEnte <me@me.com>
2021-02-20 20:12:55 +0000

    creating contacts and private routes

--- index.py
+++ index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect,url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
     return "<h1>Homepage</h1>"
+
+@app.route("/contacts")
+def contacts():
+    return "<h1>Contacts</h1>"
+
+@app.route("/private")
+def admin():
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run()
